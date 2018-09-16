e9fb586b54b569447aa02a651179210ae46ce0b2
Rene Barto <rene.barto66@gmail.com>
2018-09-16 23:04:35 +0200

    Fixes for building Windows x64 binaries

--- setup_default_options.cmake
+++ setup_default_options.cmake
@@ -26,7 +26,7 @@ macro(setup_default_options)
         # Important! do not keep warning 4251 4275 ignored!
         # Possibly switch warning 4505 back on
         set(FLAGS_CXX
-            /Wall /wd4251 /wd4275 /wd4505 /wd4514 /wd4548 /wd4571 /wd4592 /wd4625 /wd4626 /wd4710 /wd4774
+            /Wall /wd4061 /wd4251 /wd4275 /wd4505 /wd4514 /wd4548 /wd4571 /wd4592 /wd4625 /wd4626 /wd4710 /wd4774
             /wd4820 /wd5026 /wd5027 /WX- /EHsc /Gd /GR /sdl- /Zc:wchar_t /Zc:inline /fp:precise)
         set(FLAGS_CXX_DEBUG /Od /Gm- /ZI /RTC1 /MDd)
         set(FLAGS_CXX_RELEASE /Ox /GL /GS /Gy /Oi /MD)
@@ -34,7 +34,7 @@ macro(setup_default_options)
         set(FLAGS_CXX_RELWITHDEBINFO /O2 /GL /GS /Gy /Oi /Zi /MD)
 
         set(FLAGS_C
-            /Wall /wd4251 /wd4275 /wd4505 /wd4514 /wd4548 /wd4571 /wd4625 /wd4626 /wd4710 /wd4774
+            /Wall /wd4061 /wd4251 /wd4275 /wd4505 /wd4514 /wd4548 /wd4571 /wd4625 /wd4626 /wd4710 /wd4774
             /wd4820 /wd5026 /wd5027 /WX- /EHsc /Gd /GR /sdl- /Zc:wchar_t /Zc:inline /fp:precise)
         set(FLAGS_C_DEBUG /Od /Gm- /ZI /RTC1 /MDd)
         set(FLAGS_C_RELEASE /Ox /GL /GS /Gy /Oi /MD)
@@ -51,6 +51,12 @@ macro(setup_default_options)
         set(LINK_FLAGS )
         set(LINK_LIBRARIES )
 
+        # Hack to force linker to target X64 machine when needed.
+        if (${CMAKE_SIZEOF_VOID_P} EQUAL "8")
+            set(DEFINES ${DEFINES} _IA64_)
+        else ()
+            set(DEFINES ${DEFINES} _X86_)
+        endif ()
         if (MEASURE_COVERAGE)
             message(STATUS "Adding options for coverage")
         endif()
