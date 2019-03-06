d785a74418c326880b20851eb6c39d66cfad4c3a
Rene Barto <rene.barto66@gmail.com>
2019-03-06 21:58:51 +0100

    Fix rpath to correctly point to $ORIGIN

--- define_targets.cmake
+++ define_targets.cmake
@@ -22,7 +22,7 @@ function(setup_target_properties_executable target)
     endif()
     set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_BASE_DIR}/${CONFIG_DIR}/bin)
     if (UNIX)
-        set_target_properties(${target} PROPERTIES LINK_FLAGS -Wl,-rpath,'\$ORIGIN/../lib/')
+        set_target_properties(${target} PROPERTIES LINK_FLAGS -Wl,-rpath,'$$ORIGIN/../lib/')
     endif()
 endfunction()
 
